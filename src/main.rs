b907017c08ba1dc04bfc23611633d9b07f2e09e0
Jeff Fowler <jeffowler@gmail.com>
2016-10-10 22:10:23 -0400

    taking out comments

--- src/main.rs
+++ src/main.rs
@@ -19,30 +19,17 @@ fn write_header() {
 
     let numsamples = SAMPLE_RATE * 1;
 
-    // ChunkId
     handle.write(b"RIFF");
-    // ChunkSize = 36 + subchunk size 2
     handle.write_u32::<LittleEndian>(HEADER_SIZE + numsamples);
-    // Format
-    // Subchunk1ID
     handle.write(b"WAVEfmt ");
-    // Subchunk1size
     handle.write_u32::<LittleEndian>(SUBCHUNK1_SIZE);
-    // AudioFormat
     handle.write_u16::<LittleEndian>(AUDIO_FORMAT as u16);
-    // Numchannels
     handle.write_u16::<LittleEndian>(CHANNELS as u16);
-    // Samplerate
     handle.write_u32::<LittleEndian>(SAMPLE_RATE);
-    // Byterate samplerate + num of channels * bits per sample /8
     handle.write_u32::<LittleEndian>(SAMPLE_RATE * CHANNELS * (BIT_DEPTH / BYTE_SIZE));
-    // blockalign
     handle.write_u16::<LittleEndian>((CHANNELS * (BIT_DEPTH / BYTE_SIZE)) as u16);
-    // bitspersample
     handle.write_u16::<LittleEndian>(BIT_DEPTH as u16);
-    // subchunk2 id
     handle.write(b"data");
-    // subchunk2size == numsamples * numchannels * bitspersample / 8
     handle.write_u32::<LittleEndian>(numsamples * CHANNELS * (BIT_DEPTH / BYTE_SIZE));
 }
 
