ddefad320c48a5b612fb64b60a2593c4646a8704
Jeff Fowler <jeffowler@gmail.com>
2016-10-10 20:51:32 -0400

    make a single handle

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,46 @@ extern crate rand;
 
 #[allow(unused_must_use)]
 fn write_header() {
+    stdout = stdout();
+
     // ChunkId
-    stdout().write(b"RIFF");
+    stdout.write(b"RIFF");
 
     // ChunkSize = 36 + subchunk size 2
-    stdout().write(&[ 0x68, 0xac, 0x00, 0x00 ]);
+    stdout.write(&[ 0x68, 0xac, 0x00, 0x00 ]);
 
     // Format
-    stdout().write(b"WAVE");
+    stdout.write(b"WAVE");
 
     // Subchunk1ID
-    stdout().write(b"fmt ");
+    stdout.write(b"fmt ");
 
     // Subchunk1size
-    stdout().write(&[16, 0, 0, 0 ]);
+    stdout.write(&[16, 0, 0, 0 ]);
 
     // AudioFormat
-    stdout().write(&[ 1, 0 ]);
+    stdout.write(&[ 1, 0 ]);
 
     // Numchannels
-    stdout().write(&[ 1, 0 ]);
+    stdout.write(&[ 1, 0 ]);
 
     // Samplerate
-    stdout().write(&[ 0x44, 0xac, 0x00, 0x00 ]);
+    stdout.write(&[ 0x44, 0xac, 0x00, 0x00 ]);
 
     // Byterate samplerate + num of channels * bits per sample /8
-    stdout().write(&[ 0x44, 0xac, 0x00, 0x00 ]);
+    stdout.write(&[ 0x44, 0xac, 0x00, 0x00 ]);
 
     // blockalign
-    stdout().write(&[ 1, 0 ]);
+    stdout.write(&[ 1, 0 ]);
 
     // bitspersample
-    stdout().write(&[ 8, 0 ]);
+    stdout.write(&[ 8, 0 ]);
 
     // subchunk2 id
-    stdout().write(b"data");
+    stdout.write(b"data");
 
     // subchunk2size == numsamples * numchannels * bitspersample / 8
-    stdout().write(&[ 0x44, 0xac, 0x00, 0x00 ]);
+    stdout.write(&[ 0x44, 0xac, 0x00, 0x00 ]);
 }
 
 fn main() {
