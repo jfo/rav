8ab4b88f41894b30deeb15e6b0b439a58f4192b9
Jeff Fowler <jeffowler@gmail.com>
2016-10-11 23:09:30 -0400

    klabnik style notes

--- src/main.rs
+++ src/main.rs
@@ -13,27 +13,30 @@ const BIT_DEPTH: u32 = 8;
 const BYTE_SIZE: u32 = 8;
 
 #[allow(unused_must_use)]
-fn write_header<T:Write>(seconds: u32, handle: &mut T) {
+fn write_header<T: Write>(seconds: u32, handle: &mut T) {
 
     let numsamples = SAMPLE_RATE * seconds;
 
     handle.write(b"RIFF");
     handle.write_u32::<LittleEndian>(HEADER_SIZE + numsamples);
+
     handle.write(b"WAVEfmt ");
     handle.write_u32::<LittleEndian>(SUBCHUNK1_SIZE);
     handle.write_u16::<LittleEndian>(AUDIO_FORMAT as u16);
     handle.write_u16::<LittleEndian>(CHANNELS as u16);
+
     handle.write_u32::<LittleEndian>(SAMPLE_RATE);
     handle.write_u32::<LittleEndian>(SAMPLE_RATE * CHANNELS * (BIT_DEPTH / BYTE_SIZE));
     handle.write_u16::<LittleEndian>((CHANNELS * (BIT_DEPTH / BYTE_SIZE)) as u16);
     handle.write_u16::<LittleEndian>(BIT_DEPTH as u16);
+
     handle.write(b"data");
     handle.write_u32::<LittleEndian>(numsamples * CHANNELS * (BIT_DEPTH / BYTE_SIZE));
 
 }
 
 #[allow(unused_must_use)]
-fn make_some_noise<T:Write>(seconds: u32, handle: &mut T) {
+fn make_some_noise<T: Write>(seconds: u32, handle: &mut T) {
     for _ in 0..seconds * SAMPLE_RATE {
         handle.write(&[ rand::random::<u8>() ]);
     }
